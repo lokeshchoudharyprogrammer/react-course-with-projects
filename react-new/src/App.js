cda2ad6097a69f4793071e01691a30f7770dac64
Lokesh Choudhary <lokeshchoudhary2026@gmail.com>
2023-01-27 20:49:39 +0530

    1St Day

---
+++ react-new/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header";
+function App() {
+  return (
+    <>
+      <div>
+        <h1>{4 + 4}</h1>
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
